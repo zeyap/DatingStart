13a1babb95c8fd3144bcb6a269fc4928b34ef6e6
ruanjiayi <734801931@qq.com>
2018-01-28 11:48:55 +0800

    merge GameStart.cs

--- Assets/Scripts/GameStart.cs
+++ Assets/Scripts/GameStart.cs
@@ -8,17 +8,12 @@ public class GameStart : MonoBehaviour {
 	Button startBtn;
 	Text timeTxt;
 	Text startTxt;
-<<<<<<< HEAD
 	public float MaxReactionTime = 20;
     private ArrayList backgroundList = new ArrayList();
     private GameObject background;
-    // Use this for initialization
-    void Start () {
-=======
-	const float MaxReactionTime = 30;
+ 
 	// Use this for initialization
 	void Start () {
->>>>>>> b7d868e46e02c8c94c2ce6215d68da58bbbbfa78
 		startBtn = GameObject.Find ("startBtn").GetComponent<Button>();
 		timeTxt = GameObject.Find ("timeTxt").GetComponent<Text> ();
 		startTxt = GameObject.Find ("startTxt").GetComponent<Text> ();
