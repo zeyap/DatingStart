758b80d9f026739b62ef163883ba22e7aa770fcc
ruanjiayi <734801931@qq.com>
2018-01-28 11:32:29 +0800

    add scene

--- Assets/Scripts/GameStart.cs
+++ Assets/Scripts/GameStart.cs
@@ -8,15 +8,26 @@ public class GameStart : MonoBehaviour {
 	Button startBtn;
 	Text timeTxt;
 	Text startTxt;
-	const float MaxReactionTime = 20;
-	// Use this for initialization
-	void Start () {
+	public float MaxReactionTime = 20;
+    private ArrayList backgroundList = new ArrayList();
+    private GameObject background;
+    // Use this for initialization
+    void Start () {
 		startBtn = GameObject.Find ("startBtn").GetComponent<Button>();
 		timeTxt = GameObject.Find ("timeTxt").GetComponent<Text> ();
 		startTxt = GameObject.Find ("startTxt").GetComponent<Text> ();
 		Time.timeScale = 0;
 		startBtn.onClick.AddListener (OnClick);
-	}
+
+        for (int i = 1; i <= 3; i++)
+        {
+            string fileName = "scene" + i.ToString();
+            Sprite s = Resources.Load<Sprite>(fileName);
+            backgroundList.Add(s);
+        }
+        background = GameObject.Find("background");
+        background.GetComponent<SpriteRenderer>().sprite = (Sprite)backgroundList[0];
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -35,6 +46,7 @@ public class GameStart : MonoBehaviour {
 		
 	void OnClick(){
 		float level = LevelManager.GetFloatLevel ();
+        bool flag = ChangeImage.getFlag();
 		Time.timeScale = 1;
 		startBtn.gameObject.SetActive (false);
 		if (level == 1) {
@@ -42,11 +54,29 @@ public class GameStart : MonoBehaviour {
 			OrganManager.RefreshOrgans ();
 		}
 		if (level == 1.1f) {
-			LevelManager.SetFloatLevel(2.0f);
-			OrganManager.RefreshOrgans ();
+            if (flag)
+            {
+                LevelManager.SetFloatLevel(2.0f);
+                background.GetComponent<SpriteRenderer>().sprite = (Sprite)backgroundList[1];
+                ChangeImage.setFlag(false);
+            }
+            else
+            {
+                //bad scene
+            }
+            OrganManager.RefreshOrgans ();
 		}
 		if (level == 2.0f) {
-			LevelManager.SetFloatLevel(3.0f);
+            if (flag)
+            {
+                LevelManager.SetFloatLevel(3.0f);
+                background.GetComponent<SpriteRenderer>().sprite = (Sprite)backgroundList[2];
+                ChangeImage.setFlag(false);
+            }
+            else
+            {
+                //bad scene
+            }
 			OrganManager.RefreshOrgans ();
 		}
 		if (level == 3.0f) {
